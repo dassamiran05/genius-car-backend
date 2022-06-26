c92e2b686d18325b4049686f571df7aa6e0bd732
Samiran Das <dassamiran05@gmail.com>
2022-06-26 16:41:54 +0530

    adding steps

--- __server_deploy.js
+++ __server_deploy.js
@@ -7,4 +7,6 @@
  * -----------------------------
  * One time for each project
  * 1. heroku create
+ * 2. make sure git add . -> git commit -m "comment" -> git push
+ * 3. git push heroku main
  */
